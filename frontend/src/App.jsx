a758bb307f962f225e78085c94d49f75804e3352
M4ll4 <j3rmolex@gmail.com>
2025-10-23 22:55:56 -0500

    fix(routing): Inicio is now home at /, redirect /inicio and catch-all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
-import { BrowserRouter , Routes, Route} from 'react-router-dom';
+import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 
 // Components
-import Box from './components/box/Box';
 import Inicio from './components/inicio/Inicio';
+import Box from './components/box/Box';
 import Menu from './components/menu/Menu';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<Box />} />
-        <Route path='/inicio' element = {<Inicio />} />
+        {/* Home muestra Inicio */}
+        <Route path='/' element={<Inicio />} />
+        {/* Alias antiguo redirige a home */}
+        <Route path='/inicio' element={<Navigate to="/" replace />} />
         <Route path='/menu' element = {<Menu />} />
+        {/* Ruta opcional para Box si la quieres ver */}
+        <Route path='/box' element={<Box />} />
+        {/* Catch-all */}
+        <Route path='*' element={<Navigate to="/" replace />} />
 
       </Routes>
     </BrowserRouter>
